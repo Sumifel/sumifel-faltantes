8f7fe6070bb36124008a2e0a022feb60812b5160
sumifel <sumifel.loreth@gmail.com>
2026-08-04 23:40:03 -0600

    Organizar config de Prisma, limpiar next.config y restringir source de Tailwind

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from '@prisma/config';
 
 export default defineConfig({
   datasource: {
-    url: "postgresql://neondb_owner:npg_pa6VZGSU8tcK@ep-spring-salad-aynhrwqw-pooler.c-5.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+    url: "postgresql://neondb_owner:npg_pa6VZGSU8tcK@ep-spring-salad-aynhrwqw.c-5.us-east-2.aws.neon.tech/neondb?sslmode=require",
   },
 });
